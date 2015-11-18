4b77cb302c7a2d971f31c0e2d27088f1413feacd
Matt <mstetson717@gmail.com>
2015-11-18 17:37:30 -0600

    Initial Upload. Version 0.0.1

---
+++ app/src/main/java/com/mstetson/panelview_alpha/MainMenu.java
@@ -0,0 +1,25 @@
+package com.mstetson.panelview_alpha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        FullScreencall();
+    }
+
+    public void FullScreencall() {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(uiOptions);
+
+    }
+
+}
