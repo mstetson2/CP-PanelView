a3de09ceb57f04dda62bce26b79be4811c9c5f5f
Matt <mstetson717@gmail.com>
2015-11-18 19:33:10 -0600

    main menu nearing completion

--- app/src/main/java/com/mstetson/panelview_alpha/MainMenu.java
+++ app/src/main/java/com/mstetson/panelview_alpha/MainMenu.java
@@ -1,8 +1,10 @@
 package com.mstetson.panelview_alpha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -11,15 +13,20 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        FullScreencall();
+        makeFullscreen();
     }
 
-    public void FullScreencall() {
-            //for new api versions.
+    public void gotoMainMenu(View view) {
+
+            Toast.makeText(getApplicationContext(),
+                    "Button is clicked", Toast.LENGTH_LONG).show();
+    }
+
+    //Hides nav and notification bar
+    public void makeFullscreen() {
             View decorView = getWindow().getDecorView();
             int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
             decorView.setSystemUiVisibility(uiOptions);
-
     }
 
 }
